92761d4dfe518a2706d9d6771149aae377a01fa5
Mierzwagi <giihmierzwa111@outlook.com>
2024-10-08 00:06:55 -0300

    Fix: a pagination logic from API response

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,40 @@
 import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 import { IconsStyles, PageButtonsStyles, PaginationContainer } from "./style";
+import { useContextSelector } from "use-context-selector";
+import { ItensContext } from "../../pages/app/itens";
+import { useState, useMemo, useEffect } from "react";
 
 export function Pagination() {
+  const { fetchItens, selectSala, totalPages } = useContextSelector(
+    ItensContext,
+    (context) => context
+  );
+
+  const [currentPage, setCurrentPage] = useState(1);
+  /* const totalItens = useContextSelector(
+    ItensContext,
+    (context) => context.itens.length
+  ); */
+  //const totalPages = Math.ceil(totalItems / limitPerPage);
+
+  useEffect(() => {
+    if (selectSala) {
+      fetchItens(selectSala, currentPage); // Chama a busca com a sala e a página atuais
+    }
+  }, [currentPage, selectSala]);
+
+  const visiblePages = useMemo(() => {
+    const maxVisiblePages = 2;
+
+    const startPage = Math.max(1, currentPage - maxVisiblePages);
+    const endPage = Math.min(totalPages, currentPage + maxVisiblePages);
+
+    return Array.from(
+      { length: endPage - startPage + 1 },
+      (_, index) => startPage + index
+    );
+  }, [currentPage, totalPages]);
+
   return (
     <PaginationContainer>
       <IconsStyles>
@@ -9,9 +42,11 @@ export function Pagination() {
       </IconsStyles>
 
       <div>
-        <PageButtonsStyles active>1</PageButtonsStyles>
-        <PageButtonsStyles>2</PageButtonsStyles>
-        <PageButtonsStyles>3</PageButtonsStyles>
+        {visiblePages.map((page) => (
+          <PageButtonsStyles onClick={() => setCurrentPage(page)} key={page}>
+            {page}
+          </PageButtonsStyles>
+        ))}
       </div>
       <IconsStyles active>
         <CaretRight />
@@ -19,3 +54,4 @@ export function Pagination() {
     </PaginationContainer>
   );
 }
+
--- src/pages/app/itens/index.tsx
+++ src/pages/app/itens/index.tsx
@@ -9,18 +9,33 @@ import {
   SelectContainer,
   SelectInput,
 } from "./style";
+import { createContext } from "use-context-selector";
+
+interface ItensContextType {
+  limitPerPage: number;
+  fetchItens: (salaId: string, page?: number) => void;
+  itens: Item[];
+  selectSala: string;
+  totalItems: number;  // Inclua totalItems
+  totalPages: number;
+}
+
+export const ItensContext = createContext({} as ItensContextType);
 
 export function Itens() {
   const [salas, setSalas] = useState<Sala[]>([]);
   const [itens, setItens] = useState<Item[]>([]);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
   const [selectSala, setSelectSala] = useState<string>("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const limitPerPage = 10;
 
-  const fetchData = async () => {
+  const fetchSala = async () => {
     try {
       const response = await listSalas();
-      console.log(response);
+      console.log("Salas:", response);
 
       if (Array.isArray(response)) {
         setSalas(response);
@@ -37,11 +52,19 @@ export function Itens() {
   };
 
   //Item da sala selecionada
-  const fetchItens = async (salaId: string) => {
-    setLoading(true);
+  const fetchItens = async (localizacao: string, page: number = 1) => {
+    console.log(`Buscando itens para sala ${localizacao}, página ${page}`);
     try {
-      const response = await listItens(salaId);
-      setItens(response);
+      const response = await listItens(localizacao, page.toString());
+      if (response) {
+        console.log("Itens recebidos da API:", response);
+        setItens(response.data);
+        setTotalItems(response.totalItems);
+        setTotalPages(response.totalPages);
+      } else {
+        setItens([]);
+        setError("Não foi possível carregar os itens.");
+      }
     } catch (error) {
       const typedError = error as Error;
       setError(typedError.message);
@@ -51,8 +74,17 @@ export function Itens() {
   };
 
   useEffect(() => {
-    fetchData();
+    fetchSala();
   }, []);
+
+  useEffect(() => {
+    if (selectSala) {
+      console.log("Sala selecionada mudou:", selectSala);
+      fetchItens(selectSala); // Chama a busca quando a sala é selecionada
+    } else {
+      setItens([]);
+    }
+  }, [selectSala]);
 
   if (loading) {
     return <p>Carregando...</p>;
@@ -65,30 +97,37 @@ export function Itens() {
   //Criando a função para quando o usuário selecionar a sela
   const handleSalaChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const salaId = e.target.value;
+    console.log("sala selecionada", salaId);
     setSelectSala(salaId);
-    fetchItens(salaId); // Buscar itens da sala selecionada
+    //fetchItens(salaId); // Buscar itens da sala selecionada
   };
 
-  const selectedSala = salas.find((sala) => String(sala.id) === selectSala);
+  const selectedSala = salas.find(
+    (sala) => String(sala.localizacao) === selectSala
+  );
 
   return (
     <ItensContainer>
-      <HeaderContainer>
-        <IntensListContainer>
-          {selectedSala && <h3>{selectedSala.nome}</h3>}
-        </IntensListContainer>
-        <SelectContainer>
-          <SelectInput value={selectSala} onChange={handleSalaChange}>
-            {salas.map((sala) => (
-              <option key={sala.id} value={sala.id}>
-                {sala.nome}
-              </option>
-            ))}
-          </SelectInput>
-        </SelectContainer>
-      </HeaderContainer>
+      <ItensContext.Provider
+        value={{ limitPerPage, fetchItens, itens, selectSala, totalItems, totalPages }}
+      >
+        <HeaderContainer>
+          <IntensListContainer>
+            {selectedSala && <h3>{selectedSala.nome}</h3>}
+          </IntensListContainer>
+          <SelectContainer>
+            <SelectInput value={selectSala} onChange={handleSalaChange}>
+              {salas.map((sala) => (
+                <option key={sala.localizacao} value={sala.localizacao}>
+                  {sala.nome}
+                </option>
+              ))}
+            </SelectInput>
+          </SelectContainer>
+        </HeaderContainer>
 
-      <List itens={itens} />
+        <List itens={itens} />
+      </ItensContext.Provider>
     </ItensContainer>
   );
 }
